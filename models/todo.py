ab405a2db8cfb91180b4d9463ac01a32ea0b2627
ichigar <ichigar@iesanaluisabenitez.org>
2022-05-17 15:15:27 +0100

    Se añade métod para eliminar registro

--- models/todo.py
+++ models/todo.py
@@ -17,16 +17,16 @@ class Todo:
         c.close()
         return data
     
-    def get_task(self, id):
+    def get_task(self, no):
         conn = self.__connect()
         c = conn.cursor()
-        c.execute("SELECT task FROM todo WHERE id LIKE ?", (id,))
+        c.execute("SELECT task FROM todo WHERE id LIKE ?", (str(no),))
         data = c.fetchone()
         conn.commit()
         c.close()
         return data
     
-    def insert(self, task):
+    def insert_task(self, task):
         conn = self.__connect()
         c = conn.cursor()
         c.execute("INSERT INTO todo (task, status) VALUES (?,?)", (task, 1))
@@ -38,6 +38,14 @@ class Todo:
         conn = self.__connect()
         c = conn.cursor()
         c.execute("UPDATE todo SET task = ?, status = ? WHERE id LIKE ?", (task, status, no))
-        self.conn.commit()
+        conn.commit()
+        c.close()
+        return True
+    
+    def delete(self, no):
+        conn = self.__connect()
+        c = conn.cursor()
+        c.execute("DELETE FROM todo WHERE id LIKE ?", str(no))
+        conn.commit()
         c.close()
         return True
