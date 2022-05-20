bc5e5717bf5eb01f933a56b6a83b88645b5b0b79
ichigar <ichigar@iesanaluisabenitez.org>
2022-05-20 17:29:37 +0100

    uso try/execep/finalyy en consultas

--- models/todo.py
+++ models/todo.py
@@ -9,67 +9,126 @@ class Todo:
         return conn
 
     def create(self):
-        conn = self.__connect()
-        c = conn.cursor()
-        c.execute(conn.execute("CREATE TABLE todo (id INTEGER PRIMARY KEY, task char(100) NOT NULL, status bool NOT NULL)"))
-        conn.commit()
-        c.close()
-        return True
+        try:
+            conn = self.__connect()
+            c = conn.cursor()
+            c.execute(conn.execute("CREATE TABLE todo (id INTEGER PRIMARY KEY, task char(100) NOT NULL, status bool NOT NULL)"))
+            conn.commit()
+            c.close()
+        except sqlite3.Error as error:
+            print("Error while executing sqlite script", error)
+        finally:
+            if conn:
+                conn.close()
+            return True
     
     def select(self):
-        conn = self.__connect()
-        c = conn.cursor()
-        c.execute("SELECT * FROM todo")
-        data = c.fetchall()
-        conn.commit()
-        c.close()
-        return data
+        data = None
+        try:
+            conn = self.__connect()
+            c = conn.cursor()
+            c.execute("SELECT * FROM todo")
+            data = c.fetchall()
+            conn.commit()
+            c.close()
+            
+        except sqlite3.Error as error:
+            print("Error while executing sqlite script", error)
+        
+        finally:
+            if conn:
+                conn.close()
+            return data
     
     def get_task(self, no):
-        conn = self.__connect()
-        c = conn.cursor()
-        c.execute("SELECT task FROM todo WHERE id LIKE ?", (str(no),))
-        data = c.fetchone()
-        conn.commit()
-        c.close()
-        return data
+        data = None
+        try:
+            conn = self.__connect()
+            c = conn.cursor()
+            c.execute("SELECT task FROM todo WHERE id LIKE ?", (str(no),))
+            data = c.fetchone()
+            conn.commit()
+            c.close()
+        
+        except sqlite3.Error as error:
+            print("Error while executing sqlite script", error)
+        
+        finally:
+            if conn:
+                conn.close()
+            return data
     
     def insert_task(self, task):
-        conn = self.__connect()
-        c = conn.cursor()
-        c.execute("INSERT INTO todo (task, status) VALUES (?,?)", (task, 1))
-        conn.commit()
-        c.close()
-        return True
+        try:
+            conn = self.__connect()
+            c = conn.cursor()
+            c.execute("INSERT INTO todo (task, status) VALUES (?,?)", (task, 1))
+            conn.commit()
+            c.close()
+        
+        except sqlite3.Error as error:
+            print("Error while executing sqlite script", error)
+        
+        finally:
+            if conn:
+                conn.close()    
+            return True
     
     def update(self, no, task, status):
-        conn = self.__connect()
-        c = conn.cursor()
-        c.execute("UPDATE todo SET task = ?, status = ? WHERE id LIKE ?", (task, status, no))
-        conn.commit()
-        c.close()
-        return True
-    
+        try:
+            conn = self.__connect()
+            c = conn.cursor()
+            c.execute("UPDATE todo SET task = ?, status = ? WHERE id LIKE ?", (task, status, no))
+            conn.commit()
+            c.close()
+        
+        except sqlite3.Error as error:
+            print("Error while executing sqlite script", error)
+        
+        finally:
+            if conn:
+                conn.close()
+            return True
+
     def delete(self, no):
-        conn = self.__connect()
-        c = conn.cursor()
-        c.execute("DELETE FROM todo WHERE id LIKE ?", (str(no),))
-        conn.commit()
-        c.close()
-        return True
-    
+        try:
+            conn = self.__connect()
+            c = conn.cursor()
+            c.execute("DELETE FROM todo WHERE id LIKE ?", str(no))
+            conn.commit()
+            c.close()
+            
+        except sqlite3.Error as error:
+            print("Error while executing sqlite script", error)
+        
+        finally:
+            if conn:
+                conn.close()
+            return True    
     def open(self, no):
-        conn = self.__connect()
-        c = conn.cursor()
-        c.execute("UPDATE todo SET status = 1 WHERE id LIKE ?", (str(no),))
-        conn.commit()
-        c.close()
-        return True
+        try:
+            conn = self.__connect()
+            c = conn.cursor()
+            c.execute("UPDATE todo SET status = 1 WHERE id LIKE ?", (str(no),))
+            conn.commit()
+            c.close()
+        except sqlite3.Error as error:
+            print("Error while executing sqlite script", error)
+        finally:
+            if conn:
+                conn.close()    
+            return True
     
     def close(self, no):
-        conn = self.__connect()
-        c = conn.cursor()
-        c.execute("UPDATE todo SET status = 0 WHERE id LIKE ?", (str(no),))
-        conn.commit()
-        c.close()
-        return True
+        try:
+            conn = self.__connect()
+            c = conn.cursor()
+            c.execute("UPDATE todo SET status = 0 WHERE id LIKE ?", (str(no),))
+            conn.commit()
+            c.close()
+        except sqlite3.Error as error:
+            print("Error while executing sqlite script", error)
+        finally:
+            if conn:
+                conn.close()
+            return True
