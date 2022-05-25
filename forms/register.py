10c28c3878d41106ad26b7c86ccfd4d54866ff92
ichigar <ichigar@iesanaluisabenitez.org>
2022-05-25 16:10:37 +0100

    CM

--- forms/register.py
+++ forms/register.py
@@ -1,7 +1,7 @@
 from email_validator import EmailNotValidError
 from wtforms import Form, BooleanField, StringField, PasswordField, SubmitField , validators
 class RegistrationForm(Form):
-    username     = StringField('Username', [validators.Length(min=4, max=25)], default='test')
+    username     = StringField('Username', [validators.Length(min=4, max=25)], default='nombre de usaurio')
     email        = StringField('Email Address', [validators.InputRequired(), validators.Length(min=6, max=60), validators.Email()])
     accept_rules = BooleanField('Acepto las reglas del sitio', [validators.InputRequired()])
     password = PasswordField('New Password', [
